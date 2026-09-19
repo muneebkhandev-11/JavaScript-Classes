b5345ae461367cfcf60c2da4e96d257021e17eee
muneebkhandev-11 <muneebkhandev4@gmail.com>
2026-09-19 15:44:50 +0500

    Class No 1 - JavaScript Introduction

---
+++ Class No_1/index.js
@@ -0,0 +1,19 @@
+// console.log ("welcome to js")
+// console.log (2+2)
+// var a = 2
+// var b = 3 
+// console.log (a*b)
+
+// alert("hello world");
+// confirm ("do you want to xyx product");
+
+// prompt("please enter student Marks");
+ 
+// var userinput = prompt("enter student marks")
+// alert("fail go to home shabash")
+
+
+// var userage = prompt ("please enter your Age",18);
+// alert ("u are not eligable")
+
+
