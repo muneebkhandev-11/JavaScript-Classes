66825aeb779e922313bec8373c232cdfc47056f5
muneebkhandev-11 <muneebkhandev4@gmail.com>
2026-09-19 15:49:52 +0500

    Class No 3 - JavaScript Operators

---
+++ Class No_3/script.js
@@ -0,0 +1,57 @@
+//Operaters : ( operator . operant)
+
+// Operator : ( + , - , * , ** , // , %)
+
+// Operator : data on which we perform opreations
+
+// Expression : combinition of operant & operator
+// 2+ 4   =>
+// 2 ,4   => operant
+// + , - operator
+
+//  = => assignment operator
+//  == => equality (do not cheak datatype)
+// ===  => equality ( cheak datatype)
+
+// comperasion operator :
+
+/*
+
+> : grator than 
+
+> : less than 
+
+>= : grator than or equal 
+
+>= : less than or equal 
+
+!= not equal ( do not cheak datatype)
+
+!== not equal ( cheak datatype)
+
+value ++ => increment    (postfix)
+value  -- => decrement     (postfix)
+ ++ value => increment    (prefix)
+ -- value  => decrement     (prefix)
+
+
+*/
+
+// practicle work
+
+let a = 10;
+let b = 20;
+
+console.log(a + b);
+console.log(a * b);
+console.log(b / a);
+
+//  always use BODMASS RULLS
+
+let input = 3;
+console.log(input++);
+console.log(input);
+
+let value = 5;
+value = value + 2;
+console.log(value);
